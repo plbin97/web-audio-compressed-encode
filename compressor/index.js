2a500273bdd271d5f38b376081e3fd7d8b6990ad
plbin97 <i@teenet.me>
2020-10-16 11:59:26 -0400

    Temporily disabled compress

--- compressor/index.js
+++ compressor/index.js
@@ -10,22 +10,42 @@ import {audioEncode, audioDecode} from "./workerComunicator";
  * @param audioBuffer
  * @returns {Promise<Blob>}
  */
-let encoder = async (audioBuffer) => {
-    let arrayBuffer = audioBuffer.getChannelData(0);
-    let newBlob = await audioEncode(arrayBuffer);
-    return newBlob;
-};
+// let encoder = async (audioBuffer) => {
+//     let arrayBuffer = audioBuffer.getChannelData(0);
+//     let newBlob = await audioEncode(arrayBuffer);
+//     return newBlob;
+// };
 
 /**
  * Compressor Decoder
  * @param blob
  * @returns {Promise<AudioBuffer>}
  */
+// let decoder = async (blob) => {
+//
+//     let floatArr = await audioDecode(blob);
+//
+//     let audioBuffer = createAudioBufferByArrayBuffer(floatArr, 22050);
+//     // You have to return an audioBuffer object in the decoder
+//     return audioBuffer;
+// };
+
+
+let encoder = async (audioBuffer) => {
+    let arrayBuffer = audioBuffer.getChannelData(0);
+    let newBlob = new Blob([arrayBuffer]);
+    return newBlob;
+};
+
 let decoder = async (blob) => {
 
-    let floatArr = await audioDecode(blob);
+    let array = await blob.arrayBuffer();
+    // Put your code here
 
-    let audioBuffer = createAudioBufferByArrayBuffer(floatArr, 22050);
+
+    // You have to transfer your PCM data to Float32Array
+    let floatArr = new Float32Array(array);
+    let audioBuffer = createAudioBufferByArrayBuffer(floatArr, 44100);
     // You have to return an audioBuffer object in the decoder
     return audioBuffer;
 };
